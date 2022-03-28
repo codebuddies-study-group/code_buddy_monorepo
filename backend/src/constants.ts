61dfe6db787e0178a68b627165f9d58239f796a4
Luiz Barros <luizhenriquefbb@gmail.com>
2022-03-28 17:02:50 -0300

    :hammer: refactor: add API port on .env

--- backend/src/constants.ts
+++ backend/src/constants.ts
@@ -1,6 +1,6 @@
 import "dotenv/config";
 
-export const PORT = 5999;
+export const PORT = parseInt(process.env.API_PORT as string) || 5999;
 
 export const POSTGRESS_DATABASE = process.env.POSTGRESS_DATABASE as string;
 export const POSTGRESS_USERNAME = process.env.POSTGRESS_USERNAME as string;
