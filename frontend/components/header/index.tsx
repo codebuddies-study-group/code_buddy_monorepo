fd96cc2cfa73f61f94b6539fabdaa90d04b368e3
Luiz Barros <luizhenriquefbb@gmail.com>
2022-04-19 12:43:34 -0300

    ✨ feat: UI progress
    header

--- frontend/components/header/index.tsx
+++ frontend/components/header/index.tsx
@@ -1,27 +1,145 @@
-import React from 'react';
-import Head from 'next/head';
+import MenuIcon from '@mui/icons-material/Menu';
 
-const Header: React.FC = () => {
-    return (<>
-        <Head>
-            <div id='head'>
-                <h1>Code Budy</h1>
+import AppBar from '@mui/material/AppBar';
+import Avatar from '@mui/material/Avatar';
+import Box from '@mui/material/Box';
+import Button from '@mui/material/Button';
+import Container from '@mui/material/Container';
+import IconButton from '@mui/material/IconButton';
 
-                <div className='searchbar'>
-                    <span>[Search bar]</span>
-                </div>
+import Menu from '@mui/material/Menu';
+import MenuItem from '@mui/material/MenuItem';
 
-                <ul className='header-options'>
-                    <li>Meetings</li>
-                    <li>Forum</li>
-                </ul>
+import Toolbar from '@mui/material/Toolbar';
+import Tooltip from '@mui/material/Tooltip';
+import Typography from '@mui/material/Typography';
+import * as React from 'react';
+import SearchBar from '../searchbar';
 
-                <div>
-                    <span>[User Logo]</span>
-                </div>
-            </div>
-        </Head>
-    </>);
+const pages = ['Meetings', 'Forum'];
+const settings = ['Profile', 'Account', 'Dashboard', 'Logout'];
+
+const ResponsiveAppBar = () => {
+    const [anchorElNav, setAnchorElNav] = React.useState<null | HTMLElement>(null);
+    const [anchorElUser, setAnchorElUser] = React.useState<null | HTMLElement>(null);
+
+    const handleOpenNavMenu = (event: React.MouseEvent<HTMLElement>) => {
+        setAnchorElNav(event.currentTarget);
+    };
+    const handleOpenUserMenu = (event: React.MouseEvent<HTMLElement>) => {
+        setAnchorElUser(event.currentTarget);
+    };
+
+    const handleCloseNavMenu = () => {
+        setAnchorElNav(null);
+    };
+
+    const handleCloseUserMenu = () => {
+        setAnchorElUser(null);
+    };
+
+    return (
+
+        <AppBar position='static' id='head' >
+            <Container maxWidth='xl'>
+                <Toolbar disableGutters={true}>
+                    {/* Title (md+) */}
+                    <Typography
+                        variant='h6'
+                        noWrap={true}
+                        component='div'
+                        sx={{ mr: 2, display: { xs: 'none', md: 'flex' } }}
+                    >
+                        CODE BUDY
+                    </Typography>
+
+                    {/* burger menu (sm only) */}
+                    <Box sx={{ flexGrow: 1, display: { xs: 'flex', md: 'none' } }}>
+                        <IconButton
+                            size='large'
+                            aria-label='account of current user'
+                            aria-controls='menu-appbar'
+                            aria-haspopup='true'
+                            onClick={handleOpenNavMenu}
+                            color='inherit'
+                        >
+                            <MenuIcon />
+                        </IconButton>
+                        <Menu
+                            id='menu-appbar'
+                            anchorEl={anchorElNav}
+                            anchorOrigin={{
+                                vertical: 'bottom',
+                                horizontal: 'left',
+                            }}
+                            keepMounted={true}
+                            transformOrigin={{
+                                vertical: 'top',
+                                horizontal: 'left',
+                            }}
+                            open={Boolean(anchorElNav)}
+                            onClose={handleCloseNavMenu}
+                            sx={{
+                                display: { xs: 'block', md: 'none' },
+                            }}
+                        >
+                            {pages.map(page => (
+                                <MenuItem key={page} onClick={handleCloseNavMenu} sx={{
+                                    margin: 0,
+                                }}>
+                                    <Typography textAlign='center'>{page}</Typography>
+                                </MenuItem>
+                            ))}
+                        </Menu>
+                    </Box>
+
+                    {/* search bar */}
+                    <SearchBar />
+
+                    <Box sx={{ flexGrow: 1, display: { xs: 'none', md: 'flex' } }}>
+                        {pages.map(page => (
+                            <Button
+                                key={page}
+                                onClick={handleCloseNavMenu}
+                                sx={{ my: 2, color: 'white', display: 'block' }}
+                            >
+                                {page}
+                            </Button>
+                        ))}
+                    </Box>
+
+                    <Box sx={{ flexGrow: 0 }}>
+                        <Tooltip title='Open settings'>
+                            <IconButton onClick={handleOpenUserMenu} sx={{ p: 0 }}>
+                                <Avatar alt='Remy Sharp' src='/static/images/avatar/2.jpg' />
+                            </IconButton>
+                        </Tooltip>
+                        <Menu
+                            sx={{ mt: '45px' }}
+                            id='menu-appbar'
+                            anchorEl={anchorElUser}
+                            anchorOrigin={{
+                                vertical: 'top',
+                                horizontal: 'right',
+                            }}
+                            keepMounted={true}
+                            transformOrigin={{
+                                vertical: 'top',
+                                horizontal: 'right',
+                            }}
+                            open={Boolean(anchorElUser)}
+                            onClose={handleCloseUserMenu}
+                        >
+                            {settings.map(setting => (
+                                <MenuItem key={setting} onClick={handleCloseUserMenu}>
+                                    <Typography textAlign='center'>{setting}</Typography>
+                                </MenuItem>
+                            ))}
+                        </Menu>
+                    </Box>
+                </Toolbar>
+            </Container>
+        </AppBar>
+    );
 };
-
-export default Header;
+export default ResponsiveAppBar;
